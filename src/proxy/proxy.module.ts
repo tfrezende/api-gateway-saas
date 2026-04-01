0306169c72324d5de6b22a7afea38e17397e6951
Thalles Rezende <tfr.rezende@gmail.com>
2026-04-01 19:57:09 -0300

    fix: resolved import issues

--- src/proxy/proxy.module.ts
+++ src/proxy/proxy.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProxyController } from './proxy.controller';
 import { ProxyService } from './proxy.service';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, SharedModule],
   controllers: [ProxyController],
   providers: [ProxyService],
 })
